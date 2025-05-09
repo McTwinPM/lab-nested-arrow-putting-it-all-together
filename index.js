276003befb40bc536c305179697449e4d3bde736
Michael McCormack <mcctwin@gmail.com>
2025-05-08 21:10:48 -0500

    5 tests passed

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-
+function createLoginTracker(userInfo) {
+  
+  let attemptCount = 0
+  LoginAttempt = (passwordAttempt) => {
+    attemptCount+=1
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts"
+    }
+    if (passwordAttempt===userInfo.password) {
+      return "Login successful"
+    }
+    return `Attempt ${attemptCount}: Login failed`
+  }
+  return LoginAttempt
+}
 
 
 module.exports = {
